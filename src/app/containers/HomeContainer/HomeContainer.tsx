6e9fe41078520368296e99b659c16c8faa28525e
Selena Walsh Smith <ssmithgriego@gmail.com>
2024-02-28 12:12:57 -0700

    first components

---
+++ src/app/containers/HomeContainer/HomeContainer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import MainMapContainer from "../MainMapContainer/MainMapContainer";
+
+export default function HomeContainer() {
+  return (
+    <MainMapContainer />
+  );
+}
+
---
+++ src/app/containers/MainMapContainer/MainMapContainer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function MainMapContainer() {
+  return (
+    <><h1>hi</h1></>
+  );
+}
+
